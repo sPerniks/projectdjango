fc458e9eaa34da97cfbba9a165e982e78c1ad803
Pernik <pernik.alex@gmail.com>
2023-10-21 14:47:54 +0300

    lab4

--- mysite/app_blog/models.py
+++ mysite/app_blog/models.py
@@ -9,11 +9,24 @@ class Category(models.Model):
  category = models.CharField(u'Категорія',
                              max_length=250, help_text=u'Максимум 250 символів')
  slug = models.SlugField(u'Слаг', default='default-value-here')
- class Meta:
+ objects = models.Manager()
+
+
+
+class Meta:
   verbose_name = u'Категорія для публікації'
   verbose_name_plural = u'Категорії для публікацій'
- def __str__(self):
-  return self.category
+def __str__(self):
+   return self.category
+def get_absolute_url(self):
+ try:
+  url = reverse('articles-category-list',
+                kwargs={'slug': self.slug})
+ except:
+  url = "/"
+ return url
+
+
 
 
 class Article(models.Model):
--- mysite/app_blog/urls.py
+++ mysite/app_blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from app_blog import views
-from . import views
+from .views import HomePageView, ArticleDetail, ArticleList, ArticleCategoryList
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
- path(r'', views.HomePageView.as_view()),
- path('article/<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path(r'', HomePageView.as_view(), name = 'home'),
+    path(r'articles', ArticleList.as_view(), name ='articles-list'),
+    path(r'articles/category/<slug>', ArticleCategoryList.as_view(), name = 'articles-category-list'),
+    path(r'articles/<year>/<month>/<day>/<slug>', ArticleDetail.as_view(), name ='news-detail'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
